7651688c60d3baba2bd146dc39d4d2e62f65ce75
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2024-05-06 11:10:20 +0200

    project added

--- projectsData.js
+++ projectsData.js
@@ -7,7 +7,7 @@ export default {
       {
         id: uuidv4(),
         name: "FlyPlan",
-        desc: "Flyplan is an innovative and user-centric full-stack web application designed to deliver seamless and responsive flight information in real-time. With its intuitive interface, users can effortlessly access and track flight details, while also enjoying the convenience of saving personalized preferences and updates within their personal accounts. Stay informed and connected with Flyplan as it brings you the latest flight information, all wrapped in a user-friendly experience",
+        desc: "Discover the future of flight tracking with FlyPlan. This cutting-edge, full-stack web application offers real-time flight information with an emphasis on user-centric design. Its sleek interface allows for easy navigation and personalized updates, making flight tracking an effortless part of your travel plans. Whether you're a frequent flyer or an aviation enthusiast, FlyPlan ensures you stay ahead with the latest in flight technology.",
         tech: "JavaScript | TypeScript |React | Node.js | HTML | CSS | MongoDB | Skyscanner API",
         img: "/assets/flyplan.png",
         video_link: "https://drive.google.com/file/d/1hGZ-fBxTkMHY1aaL-EigioTvRpWHyujF/view?usp=share_link",
@@ -16,19 +16,28 @@ export default {
       {
         id: uuidv4(),
         name: "Word-Mind",
-        desc: "Word-Mind is Flashcard App to learn language word quickly",
-        tech: "JavaScript |React | Node.js | HTML | Tailwind | Firebase ",
+        desc: "Elevate your language learning with Word-Mind, a dynamic flashcard app designed to help you master new words swiftly and effectively. Utilizing the latest web technologies, Word-Mind creates a personalized learning environment that adapts to your pace and style. Ideal for students, professionals, and casual learners alike, it's the perfect tool to expand your vocabulary across various languages.",
+        tech: "JavaScript |React | Node.js | HTML | Tailwind | Firebase",
         img: "/assets/word-mind.png",
         link: "https://word-mind-mustafa-altuntas.vercel.app/",
       },
       {
         id: uuidv4(),
+        name: "OZJewels",
+        desc: "Step into the world of luxury with OZJewels, a premium Shopify-powered jewelry shop. This online store showcases a carefully curated selection of fine jewelry, offering an elegant and responsive shopping experience. From browsing to checkout, OZJewels makes luxury accessible, combining beautiful design with intuitive functionality to meet the highest standards of online retail.",
+        tech: "Shopify | JavaScript | HTML | CSS",
+        img: "/assets/Ozjewels.png",
+        link: "https://www.ozjewels.nl/",
+      },
+      {
+        id: uuidv4(),
         name: "Games",
-        desc: "Some small games created for freelance work. You can have fun with snake and pong game",
-        tech: "React | JavaScript  | CSS ",
+        desc: "Dive into nostalgia with our custom-built games platform, featuring classics like Snake and Pong. Designed for quick leisurely breaks or engaging play sessions, these games are crafted to bring simple joy and fun directly to your browser. Perfect for all ages, they offer a blend of retro aesthetics with modern web technology to deliver smooth and engaging gameplay.",
+        tech: "React | JavaScript | CSS",
         img: "/assets/games.png",
         link: "https://snake-pong-games.vercel.app",
       },
+      
     ],
   },
 };
